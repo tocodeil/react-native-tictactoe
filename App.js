d5887fc08bb0749c131651f5b4144d34bb282f19
ynonp <ynonperek@gmail.com>
2022-07-24 12:07:52 +0300

    part2

--- App.js
+++ App.js
@@ -1,7 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
 
-export default function App() {
+export default function MainMenu() {
+  function play(players) {
+    alert(`Play: ${players}`);
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Welcome To my Tic Tac Toe Game</Text>
+      </View>
+      <View style={styles.main}>
+        <Pressable onPress={() => play(1)} style={styles.button}>
+          <Text style={styles.buttonText}>1 Player</Text>
+        </Pressable>
+        <Pressable onPress={() => play(2)} style={styles.button}>
+          <Text style={styles.buttonText}>2 Players</Text>
+        </Pressable>
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+
+function App() {
   return (
     <View style={styles.container}>
       <Text>I'm in the iOS Simulator</Text>
@@ -11,10 +35,31 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
+  header: {
+    height: 20,
+  },
+  headerText: {
+    fontSize: 36,
+  },
+  main: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  buttonText: {
+    textAlign: 'center',
+  },
+  button: {
+    backgroundColor: '#c9c9c9',
+    margin: 5,
+    width: 150,
+    padding: 10,
+    borderRadius: 10,
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
   },
 });
+
+
