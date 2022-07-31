3e4405f111ac33fef1c4b2790c60c13d533fe4b6
ynonp <ynonperek@gmail.com>
2022-07-31 11:40:30 +0300

    part 3

--- App.js
+++ App.js
@@ -1,7 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Pressable } from 'react-native';
+import _ from 'lodash';
 
-export default function MainMenu() {
+export default function Game() {
+  function play(row, column) {
+    alert(`Play: ${row}, ${column}`);
+  }
+
+  return (
+    <View style={gameStyles.container}>
+      {
+        _.range(3).map(row => (
+          <View key={row} style={gameStyles.row}>
+            {
+              _.range(3).map(column => (
+                <Pressable style={gameStyles.cell} key={column} onPress={() => play(row, column)}>
+                  <Text style={gameStyles.cellText}> </Text>
+                </Pressable>
+              ))}
+          </View>
+        ))}
+    </View>
+  );
+}
+
+const gameStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  cellText: {
+    fontSize: 24,
+  },
+  cell: {
+    flex: 1,
+    margin: 5,
+    backgroundColor: '#f1d2c3',
+  },
+});
+
+
+function MainMenu() {
   function play(players) {
     alert(`Play: ${players}`);
   }
